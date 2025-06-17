b48ddf2f75061ab49570b576e25d9c5e7766989a
Roslyn Lewis <139125575+Roslyn10@users.noreply.github.com>
2025-06-17 21:11:55 +0200

    Adding first part of week 7 which is an intro to strings

---
+++ week_7/strings.py
@@ -0,0 +1,11 @@
+#This is a file on Strings
+
+singlequotes = 'Hello World'
+print(singlequotes)
+
+doublequotes = "Hello World"
+print(doublequotes)
+
+multiplelines = """This is going
+over multiple lines"""
+print(multiplelines)
