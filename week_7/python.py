7494237e994f4266ba04feff0d4b082a12094bfd
Roslyn Lewis <139125575+Roslyn10@users.noreply.github.com>
2025-06-24 15:01:38 +0200

    Adding the basics od numeric data

---
+++ week_7/python.py
@@ -0,0 +1,10 @@
+#This file contains lessons on how to work with numeric data
+
+num = 3
+
+print(num) #Prints the value of the variable
+print(type(num)) #Prints the variable type
+
+num2 = 3.14
+
+print(type(num2)) #Prints the variable type
