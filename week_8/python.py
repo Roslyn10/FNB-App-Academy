9d5ac64a6734180a94e860ac0f4f5513cbb091e0
Roslyn Lewis <139125575+Roslyn10@users.noreply.github.com>
2025-07-01 14:23:47 +0200

    Adding more information to the lists

--- week_8/python.py
+++ week_8/python.py
@@ -7,3 +7,24 @@ fruits[1] = "blueberry"
 print(fruits[1])
 print(fruits)
 
+# adding items to a list 
+
+vegetables = ["carrot", "potatoes", "lettuce"]
+vegetables.append("leeks")
+print(vegetables)
+
+vegetables.insert(1, "kale")
+
+print(vegetables)
+
+# Removing items from the list
+vegetables.remove("kale")
+print(vegetables)
+
+#Sorting the list in alphabetical order
+vegetables.sort()
+print(vegetables)
+
+#Sorting the list in reverse alphabetical order
+vegetables.sort(reverse=True)
+print(vegetables)
