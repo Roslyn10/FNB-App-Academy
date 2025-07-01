f27b835920e5f3d91d7fc1a25e0a4d9ae7f49106
Roslyn Lewis <139125575+Roslyn10@users.noreply.github.com>
2025-07-01 15:50:13 +0200

    Learning the basics of Dictionaries

---
+++ week_8/dictionaries.py
@@ -0,0 +1,13 @@
+#dictionaries  indexed by keys of any type
+
+my_dict = {'apple': 3, 'banana': 5, 'orange': 2}
+print(my_dict)
+print(my_dict['apple']) # Prints the value assocaiated with the value 
+
+# Adding a value to a dictionary
+my_dict['grapes'] = 4
+print(my_dict)
+
+# Modify an exsisting value
+my_dict['banana'] = 6
+print(my_dict)
