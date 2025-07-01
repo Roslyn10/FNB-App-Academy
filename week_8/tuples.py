10d524680b299e8c43dcedc54820ecdd052a8d4e
Roslyn Lewis <139125575+Roslyn10@users.noreply.github.com>
2025-07-01 15:15:33 +0200

    Adding the file with the basics of tuples

---
+++ week_8/tuples.py
@@ -0,0 +1,15 @@
+#Tuples, ordered and unchangeable
+
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple)
+print(my_tuple[0])
+print(my_tuple[-3])
+
+# Concatonating the tuple
+tuple1 = (1, 2, 3, 4)
+tuple2 = (5, 6, 7, 8)
+rep_tuple = tuple1 * 3
+
+conc_tuple = tuple1 + tuple2
+print(conc_tuple)
+print(rep_tuple)
