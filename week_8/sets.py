cd5417c99f483df0968148213b05c9b344010e86
Roslyn Lewis <139125575+Roslyn10@users.noreply.github.com>
2025-07-07 20:37:00 +0200

    Fixing some issues in sets.py, and adding modules

--- week_8/sets.py
+++ week_8/sets.py
@@ -1,4 +1,4 @@
-# Sets
+#Sets
 
 my_set = {1, 2, 3, 4, 5, 6}
 print(my_set)
---
+++ week_9/modules.py
@@ -0,0 +1,7 @@
+# Modules
+
+import math_operations
+
+print(math_operations.add(6, 90))
+
+print(math_operations.sub(9, 6))
